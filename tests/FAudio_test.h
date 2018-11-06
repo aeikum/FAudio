6f5a6a1f2523061979d197bd8b479f4c46b54072
Andrew Eikum <aeikum@codeweavers.com>
2018-11-06 14:12:33 -0600

    tests: Initial

---
+++ tests/FAudio_test.h
@@ -0,0 +1,6 @@
+typedef uint32_t HRESULT;
+typedef FAudio IXAudio27;
+#define IXAudio27_Initialize FAudio_Initialize
+#define IXAudio27_Release FAudio_Release
+#define S_OK 0
+#define XAUDIO2_ANY_PROCESSOR FAUDIO_DEFAULT_PROCESSOR
